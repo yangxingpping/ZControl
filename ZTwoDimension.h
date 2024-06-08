fbb54b06aeae2ea41cbdd8177983cf96dc350ce9
ggyy <alqaz@outlook.com>
2024-06-08 18:49:24 +0800

    update

--- ZTwoDimension.h
+++ ZTwoDimension.h
@@ -11,56 +11,69 @@ using std::map;
 using std::unique_ptr;
 using std::vector;
 
-template<typename RowItem, typename ClientRoles>
-class ZRoundButtonItem : public QAbstractListModel
+template<typename RowItem>
+class ZTwoDimension : public QAbstractListModel
 {
-    Q_OBJECT
-    QML_ELEMENT
+	Q_OBJECT
+		//QML_ELEMENT
 public:
-    static_assert(std::is_enum_v<ClientRoles>);
-    explicit ZRoundButtonItem(map<int, QByteArray> sysRoles, QObject *parent = nullptr)
-        :QAbstractListModel(parent)
-        ,_roles(sysRoles)
-    {
-        if(!sysRoles.empty())
-        {
-            assert(sysRoles.rbegin()->first < Qt::UserRole);
-        }
-		constexpr auto roleEntries = magic_enum::enum_entries<ClientRoles>();
-        for(auto&& kv : roleEntries)
-        {
-            _roles.insert({ magic_enum::enum_integer(kv.first), magic_enum::enum_name(kv.second) });
-        }
-    }
-    virtual ~ZRoundButtonItem() override
-    {
-    }
-    int rowCount(const QModelIndex &index = QModelIndex()) const override
-    {
-        auto pret = _d.count();
-        int ret = static_cast<int>(pret);
-        return ret;
-    }
-    QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override
-    {
-        auto rowindx = index.row();
-        auto it = _d.find(rowindx);
-        if(it==_d.end())
-        {
-            return QVariant();
-        }
-        if(role< Qt::UserRole)
-        {
-            
-        }
-    }
-    QHash<int, QByteArray> roleNames() const override
-    {
-        return _roles;
-    }
+	using ClientRole = typename RowItem::ClientRole;
+	static_assert(std::is_enum_v<typename RowItem::ClientRole>);
+	explicit ZTwoDimension(map<int, QByteArray> sysRoles, QObject* parent = nullptr)
+		:QAbstractListModel(parent)
+	{
+		if (!sysRoles.empty())
+		{
+			assert(sysRoles.rbegin()->first < Qt::UserRole);
+		}
+		for (auto&& kv : sysRoles)
+		{
+			_roles.insert(kv.first, kv.second);
+		}
+		constexpr auto roleEntries = magic_enum::enum_entries<ClientRole>();
+		for (auto&& kv : roleEntries)
+		{
+			//_roles.insert(magic_enum::enum_integer(kv.first), magic_enum::enum_name(kv.second));
+		}
+	}
+	virtual ~ZTwoDimension() override
+	{
+	}
+	int rowCount(const QModelIndex& index = QModelIndex()) const override
+	{
+		auto pret = _d.size();
+		int ret = static_cast<int>(pret);
+		return ret;
+	}
+	QVariant data(const QModelIndex& index, int role = Qt::DisplayRole) const override
+	{
+		auto rowindx = index.row();
+		auto it = _d.find(rowindx);
+		if (it == _d.end())
+		{
+			return QVariant();
+		}
+		if (role < Qt::UserRole)
+		{
+			return it->second->getSys(role);
+		}
+		else
+		{
+			auto opte = magic_enum::enum_cast<ClientRole>(role - Qt::UserRole);
+			if (!opte.has_value())
+			{
+				return QVariant();
+			}
+			return it->second->getUser(opte.value());
+		}
+	}
+	QHash<int, QByteArray> roleNames() const override
+	{
+		return _roles;
+	}
 Q_SIGNALS:
 
 public:
-    map<int, unique_ptr<RowItem>> _d;
-    map<int, QByteArray> _roles;
+	map<int, unique_ptr<RowItem>> _d;
+	QHash<int, QByteArray> _roles;
 };
--- main.cpp
+++ main.cpp
@@ -4,12 +4,38 @@
 #include <QQmlContext>
 #include "ZTwoDimension.h"
 
+class Demo
+{
+public:
+	enum class ClientRole
+	{
+		Role1,
+		Role2,
+	};
+
+	QVariant getSys(int role)
+	{
+		return "name";
+	}
+	QVariant getUser(ClientRole role)
+	{
+		return "age";
+	}
+};
+
+//using TypeDemo = ZTwoDimension<Demo>;
+
 int main(int argc, char* argv[])
 {
+
+	//qRegisterMetaType<ZTwoDimension<Demo>>("TypeDemo");
+
 	QGuiApplication app(argc, argv);
 
 	QQmlApplicationEngine engine;
 
+	//auto pdemo = new TypeDemo({}, &app);
+	//engine.rootContext()->setContextProperty("abc", pdemo);
 	QObject::connect(
 		&engine,
 		&QQmlApplicationEngine::objectCreationFailed,
